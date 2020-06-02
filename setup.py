9b6c5b2df09ab5876424f052556d6a41c8ba9f24
martinRenou <martin.renou@gmail.com>
2020-06-02 10:03:07 +0200

    Release 0.3.0
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ for (dirpath, dirnames, filenames) in os.walk('share/jupyter/voila/templates/'):
 
 setup(
     name='voila-material',
-    version="0.2.5",
+    version="0.3.0",
     description="Material design template for voila",
     data_files=data_files,
     include_package_data=True,
