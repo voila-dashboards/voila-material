dff515f48bd9c0d61172001e99817749b37921ce
martinRenou <martin.renou@gmail.com>
2023-03-07 11:32:06 +0100

    Release 0.4.2

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ for (dirpath, dirnames, filenames) in os.walk('share/jupyter/'):
 
 setup(
     name='voila-material',
-    version="0.4.1",
+    version="0.4.2",
     description="Material design template for voila",
     data_files=data_files,
     include_package_data=True,
